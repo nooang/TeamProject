51fa13396904beecc3e5aa967414ec2993317c76
211-09\User <qnprk55@gmail.com>
2023-10-06 15:48:01 +0900

    tt

--- TeamProject2/src/main/java/com/ktdsuniversity/edu/companybbs/ComppostVO.java
+++ TeamProject2/src/main/java/com/ktdsuniversity/edu/companybbs/ComppostVO.java
@@ -1,5 +1,19 @@
 package com.ktdsuniversity.edu.companybbs;
 
 public class ComppostVO {
-
+	private String compPostId;
+	private String postWriId;
+	private String boardId;
+	private String postTitle;
+	private String postCont;
+	private String crtDt;
+	private String recrStDt;
+	private String recrFinDt;
+	private String recrProgVn;
+	private String recrForm;
+	private int minExpYear;
+	private int maxExpYear;
+	private int viewCnt;
+	private int likeCnt;
+	private String delYn;
 }
